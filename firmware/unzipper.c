0011bcac336a5d9ca70b64f4b73b24799b4cd16e
thireo <andreas@thirsbro.dk>
2021-08-24 21:47:59 +0200

    added get_dentry()

--- firmware/unzipper.c
+++ firmware/unzipper.c
@@ -9,21 +9,24 @@ union byte4 {
         float numfloat;
 };
 int endianSwap4int(int a);
+u32 get_dentry(struct u5fs_dentry * dest, u8 * src);
 
 int main(void)
 {
     u32 buffer;
     FILE * file_pointer = fopen("firmware.img","rb");
     int i = 0;
-    printf("%lu",(u32) endianSwap4int(U5FS_SUPERBLOCK_MAGIC));
-    
-    
+    //printf("%lu",(u32) endianSwap4int(U5FS_SUPERBLOCK_MAGIC));
+
     u32 sb_addr = 0;
     u32 sb_ver = 0;
     u32 sb_size = 0;
     u32 sb_count = 0;
     u32 root_addr = 0;
     u32 bm_count = 0;
+
+    struct u5fs_superblock_v1 sb;
+
 
     while(fread(&buffer,1,4,file_pointer) == 4)
     {
@@ -38,46 +41,46 @@ int main(void)
         else if(sb_addr != 0 && i == sb_addr+1)
         {
             printf("\n%lu : 0x%08x",i, endianSwap4int(buffer)); // prints a series of bytes
-            sb_ver = endianSwap4int(buffer);
+            sb.version = endianSwap4int(buffer);
         }
         else if(sb_addr != 0 && i == sb_addr+2)
         {
             printf("\n%lu : 0x%08x",i, endianSwap4int(buffer)); // prints a series of bytes
-            sb_size = endianSwap4int(buffer);
+            sb.blocksize = endianSwap4int(buffer);
         }
         else if(sb_addr != 0 && i == sb_addr+3)
         {
             printf("\n%lu : 0x%08x",i, endianSwap4int(buffer)); // prints a series of bytes
-            sb_count = endianSwap4int(buffer);
+            sb.blockcount = endianSwap4int(buffer);
         }
         else if(sb_addr != 0 && i == sb_addr+4)
         {
             printf("\n%lu : 0x%08x",i, endianSwap4int(buffer)); // prints a series of bytes
-            root_addr = endianSwap4int(buffer);
+            sb.rootnode = endianSwap4int(buffer);
 
             printf("\n---------------------\n");
             printf("SUPERBLOCK @\t%lu\n",sb_addr);
-            printf("Version:\t%lu\n",sb_ver);
-            printf("Block size:\t%lu\n",sb_size);
-            printf("Block count:\t%lu\n",sb_count);
-            bm_count = sb_count/(sb_size*8) + (sb_count % (sb_size*8) != 0); // Fast ceiling method returning int.
+            printf("Version:\t%lu\n",sb.version);
+            printf("Block size:\t%lu\n",sb.blocksize);
+            printf("Block count:\t%lu\n",sb.blockcount);
+            bm_count = sb.blockcount/(sb.blocksize*8) + (sb.blockcount % (sb.blocksize*8) != 0); // Fast ceiling method returning int.
             printf("Bitmap Blocks:\t%lu\n",bm_count);
-            printf("Rootnode:\t%lu @ 0x%08x\n",root_addr,root_addr*sb_size+sb_addr*4);
+            printf("Rootnode:\t%lu @ 0x%08x\n",sb.rootnode,sb.rootnode*sb.blocksize+sb_addr*4);
             printf("---------------------\n");
-
+            break;
         }
-        else if(sb_addr != 0 && i == sb_addr+sb_size)
+        /*else if(sb_addr != 0 && i == sb_addr+sb.blocksize)
         {
             printf("\n%lu : 0x%08x",i, endianSwap4int(buffer)); // prints a series of bytes
-            u32 bitmaps[bm_count*sb_size];
-            for(int k=0;k<sb_size;k++)
+            u32 bitmaps[bm_count*sb.blocksize];
+            for(int k=0;k<sb.blocksize;k++)
             {
                 fread(&buffer,1,4,file_pointer);
                 bitmaps[k] = endianSwap4int(buffer);
                 //printf(BYTE_TO_BINARY_PATTERN"\n",BYTE_TO_BINARY(bitmaps[k]));
             }
         }
-        else if(root_addr != 0 && i == root_addr*sb_size+sb_addr*4)
+        else if(root_addr != 0 && i == root_addr*sb.blocksize+sb_addr*4)
         {
             printf("\n%lu : 0x%08x\n",i, endianSwap4int(buffer)); // prints a series of bytes
             struct u5fs_dir_inode rootnode;
@@ -126,11 +129,83 @@ int main(void)
             printf("0x%08x\n",temp);
 
             return 1;
-        }
-        
+        }*/
         i++;
     }
+
+    printf("\n----------0x%08x--------",ftell(file_pointer));
+    fseek(file_pointer,sb_addr*4+sb.blocksize,SEEK_SET);
+    printf("\n----------0x%08x--------",ftell(file_pointer));
+
+    u32 bitmaps[bm_count*sb.blocksize];
+    for(int k=0;k<sb.blocksize;k++)
+    {
+        fread(&buffer,1,4,file_pointer);
+        bitmaps[k] = endianSwap4int(buffer);
+        //printf(BYTE_TO_BINARY_PATTERN"\n",BYTE_TO_BINARY(bitmaps[k]));
+    }
+
+
+    
+
+    fseek(file_pointer,sb_addr*4+sb.blocksize*sb.rootnode,SEEK_SET);
+    printf("\n----------0x%08x--------",ftell(file_pointer));
+
+    u8 *tempdata = (u8 *) malloc(sb.blocksize);
+    fread(tempdata,1,sb.blocksize,file_pointer);
+    for(int h=0;h<sb.blocksize;h++)
+    {
+        //tempdata[h] = endianSwap4int(tempdata[h]);
+        //printf("\n:::::::0x%08x::::::",tempdata[h]);
+    }
+
+    struct u5fs_dir_inode rootnode;
+
+    memcpy(&rootnode,tempdata,sizeof(rootnode));
+
+
+    printf("\n0x%08x",rootnode.header.size);
+    
+    
+    u32 index = 0;
+    struct u5fs_dentry temp;
+    /*memcpy(&temp,&tempdata[sizeof(rootnode)],5);
+    int length = sscanf((u8 *)&tempdata[sizeof(rootnode)+5],"%s",temp.name);
+    temp.dnode = endianSwap4int(temp.dnode);*/
+    index += get_dentry(&temp,&tempdata[sizeof(rootnode)]);
+    printf("\n...........%lu,%u,%s...........",temp.dnode,temp.dtype,temp.name);    
+    
+    struct u5fs_dentry temp2;
+    index += get_dentry(&temp2,&tempdata[sizeof(rootnode)+index]);
+    printf("\n...........%lu,%u,%s...........",temp2.dnode,temp2.dtype,temp2.name);
+
+
+    struct u5fs_dentry temp3;
+    index += get_dentry(&temp3,&tempdata[sizeof(rootnode)+index]);
+    printf("\n...........%lu,%u,%s...........",temp3.dnode,temp3.dtype,temp3.name);
+
+    struct u5fs_dentry temp4;
+    index += get_dentry(&temp4,&tempdata[sizeof(rootnode)+index]);
+    printf("\n...........%lu,%u,%s...........",temp4.dnode,temp4.dtype,temp4.name);
+
+    struct u5fs_dentry temp5;
+    index += get_dentry(&temp5,&tempdata[sizeof(rootnode)+index]);
+    printf("\n...........%lu,%u,%s...........",temp5.dnode,temp5.dtype,temp5.name);
+    
+    return 1;
+    
+    
+    printf("...........%lu,%u,%s...........",temp.dnode,temp.dtype,temp.name);
+
     fclose(file_pointer);
+}
+
+u32 get_dentry(struct u5fs_dentry * dest, u8 * src)
+{
+    memcpy(dest,src,5);
+    int length = sscanf((u8 *)src+5,"%s",dest->name);
+    dest->dnode = endianSwap4int(dest->dnode);
+    return 5+strlen(dest->name)+1;
 }
 
 
